73b55f1a8e3c961da2e443704e4b94007d82bf80
Jose Rodriguez <jose.rodriguez@criarenet.com>
2025-09-15 14:14:26 -0300

    Alterando arquivo configuração no database.php para o Redis

--- config/database.php
+++ config/database.php
@@ -143,7 +143,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -170,10 +170,10 @@ return [
         ],
 
         'conversations' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => 2,
+            'host' => env('NF_REDISASSISTENTE_HOST', '127.0.0.1'),
+            'password' => env('NF_REDISASSISTENTE_PASSWORD', null),
+            'port' => env('NF_REDISASSISTENTE_PORT', '6379'),
+            'database' => 0,
             'prefix' => 'conversations:',
         ]
 
